ac5c0bf572a2259c9f0af7ee3e0192ed9480053d
Djkde01 <sergio.estrella@utp.edu.co>
2022-10-14 00:44:21 -0500

    chore(Updated tests)

--- test.js
+++ test.js
@@ -1,19 +1,31 @@
 const checkConsecutive = require("./index.js");
 
 describe("Should be consecutive arrays", () => {
-  it("Remove only one", () => {
-    expect(checkConsecutive([1, 2, 3, 4, 5, 6, 7, 9])).toBe(true);
-  });
   it("Not removes anything", () => {
-    expect(checkConsecutive([1, 2, 3, 2, 4, 5, 6, 7])).toBe(true);
+    expect(checkConsecutive([1, 2, 3, 4, 5, 6, 7, 9])).toMatchObject({
+      isConsecutive: true,
+      array: [1, 2, 3, 4, 5, 6, 7, 9],
+    });
+  });
+  it("Remove only one", () => {
+    expect(checkConsecutive([1, 2, 3, 2, 4, 5, 6, 7])).toMatchObject({
+      isConsecutive: true,
+      array: [1, 2, 3, 4, 5, 6, 7],
+    });
   });
 });
 
 describe("Should NOT be consecutive arrays", () => {
   it("Fails on 2", () => {
-    expect(checkConsecutive([1, 2, 2, 2, 4, 5, 6, 7])).toBe(false);
+    expect(checkConsecutive([1, 2, 2, 2, 4, 5, 6, 7])).toMatchObject({
+      isConsecutive: false,
+      array: [1, 2, 2, 2, 4, 5, 6, 7],
+    });
   });
   it("Fails on 1", () => {
-    expect(checkConsecutive([1, 2, 3, 1, 1, 4, 8, 9])).toBe(false);
+    expect(checkConsecutive([1, 2, 3, 1, 1, 4, 8, 9])).toMatchObject({
+      isConsecutive: false,
+      array: [1, 2, 3, 1, 1, 4, 8, 9],
+    });
   });
 });
