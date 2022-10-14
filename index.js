fd7726384018beb0085eacb1e5c8943af0e45596
Djkde01 <sergio.estrella@utp.edu.co>
2022-10-13 23:25:44 -0500

    Initial config

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+[1, 2, 3, 2, 4, 5, 6, 7] Eliminar 1 número cumple la condición.
+[1, 2, 2, 2, 4, 5, 6, 7] Eliminar 1 número no cumple la condición. ( 2 < 2 ) = false
+[1, 2, 3, 1, 1, 4, 8, 9] Eliminar 1 número no cumple la condición.
+[1, 2, 3, 4, 5, 6, 7, 9] Cumple la condición sin necesidad de eliminar alguno
+*/
+
+const checkConsecutive = (numbersArray) => {};
+
+module.exports = checkConsecutive;
